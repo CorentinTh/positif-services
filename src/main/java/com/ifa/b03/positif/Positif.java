888377ad37994b0d716575cce44f24b29d060b3d
Corentin Thomasset <cthomasset@insa-lyon.fr>
2019-05-27 11:04:15 +0200

    feat: service getInfoClient

---
+++ src/main/java/com/ifa/b03/positif/Positif.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ifa.b03.positif;
+
+import com.ifa.b03.positif.entities.Client;
+import com.ifa.b03.positif.dao.ClientDao;
+import com.ifa.b03.positif.dao.JpaUtil;
+import com.ifa.b03.positif.services.Services;
+
+/**
+ *
+ * @author cthomasset
+ */
+public class Positif {
+    public static void main (String[] args){
+        System.out.println("Hello World");
+
+        JpaUtil.init();
+
+
+        JpaUtil.createEntityManager();
+        JpaUtil.openTransaction();
+
+        Client c = new Client();
+        c.setFirstname("Bob");  
+        ClientDao.persist(c);
+
+        JpaUtil.validateTransaction();
+        JpaUtil.closeEntityManager();
+        
+        System.out.println(Services.getInfoClient(new Long(1)).getFirstname());
+        
+        JpaUtil.destroy();
+    }
+}
---
+++ src/main/java/com/ifa/b03/positif/dao/ClientDao.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ifa.b03.positif.dao;
+
+import com.ifa.b03.positif.entities.Client;
+import javax.persistence.EntityManager;
+
+/**
+ *
+ * @author cthomasset
+ */
+public class ClientDao{
+    
+    public static void persist(Client client){
+        JpaUtil.getEntityManager().persist(client);
+    }
+    
+    public static Client getClientById(Long id){
+        return (Client) JpaUtil
+                .getEntityManager()
+                .find(Client.class, id);
+    }
+    
+    
+}
