9f2428a87a9b5a8b7277fa9a63256bc866b20af6
Corentin Thomasset <cthomasset@insa-lyon.fr>
2019-05-27 08:52:29 +0200

    feat: hello world

---
+++ src/main/java/com/ifa/bxx/positif/Positif.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ifa.bxx.positif;
+
+/**
+ *
+ * @author cthomasset
+ */
+public class Positif {
+    public static void main (String[] args){
+        System.out.println("Hello World");
+    }
+}
